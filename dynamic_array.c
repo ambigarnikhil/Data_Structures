b410bc2342a875775890f43854b6a00fb20088a5
Nikhil Ambigar <70022502+ambigarnikhil@users.noreply.github.com>
2021-01-24 19:02:01 +0530

    Add files via upload

---
+++ dynamic_array.c
@@ -0,0 +1,30 @@
+/*Create a array dynamically and store all the elements based on user input and display the array*/
+
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+    int n;
+    int *arr;
+    printf("Enter the number of elements:\n");
+    scanf("%d",&n);
+    arr=(int*)calloc(n,sizeof(int));
+    adding_elements(n,arr);
+    display(n,arr);
+}
+void adding_elements(int n,int *arr)
+{
+    for(int i=0;i<n;i++)
+    {
+        printf("Enter the %d element:",i+1);
+        scanf("%d",arr+i);
+    }
+}
+void display(int n,int *arr)
+{
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",*(arr+i));
+    }
+
+}
